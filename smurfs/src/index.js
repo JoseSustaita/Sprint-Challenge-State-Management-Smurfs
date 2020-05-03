6753570b5f034d3bc3b2170adf0dd262ced48532
Jose Sustaita <josesustaita70@gmail.com>
2020-05-03 19:25:26 -0400

    Action and reducers added

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -5,6 +5,7 @@ import App from "./components/App";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import { reducer } from "./reducers/reducer";
 
 const store = createStore(reducer, applyMiddleware(thunk));
 ReactDOM.render(
